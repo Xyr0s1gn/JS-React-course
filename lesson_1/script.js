5097f21a387ffa9bdf180547517b26323ca41eef
Bino Faata <maximleluk@gmail.com>
2020-04-27 03:23:00 +0300

    Script updated. prompt messages inserted inside loop. added type check to user answers. added social variants to budget calc

--- lesson_1/script.js
+++ lesson_1/script.js
@@ -1,21 +1,43 @@
-// 'use strict';
-// let money = prompt('Ваш бюджет на месяц?');
-// let time = prompt('Введите дату в формате YYYY-MM-DD');
+'use strict';
 
-// let appData = {
-//   budget: money,
-//   timeData: time,
-//   expenses: {},
-//   optionalExpenses: {},
-//   income: [],
-//   savings: false,
-// };
-// let buy1 = prompt('Введите обязательную статью расходов в этом месяце');
-// let buy1Price = prompt('Во сколько обойдется ' + 'buy1 ' + '?');
-// let buy2 = prompt('Введите обязательную статью расходов в этом месяце');
-// let buy2Price = prompt('Во сколько обойдется ' + 'buy2 ' + '?');
+let money = +prompt('Ваш бюджет на месяц?');
+let time = prompt('Введите дату в формате YYYY-MM-DD');
 
-// appData.expenses[buy1] = buy1Price;
-// appData.expenses[buy2] = buy2Price;
+let appData = {
+  budget: money,
+  timeData: time,
+  expenses: {},
+  optionalExpenses: {},
+  income: [],
+  savings: false,
+};
 
-// alert('бюджет на 1 день - ' + money / 30);
+for (let i = 0; i < 2; i++) {
+  let a = prompt('Введите обязательную статью расходов в этом месяце', ''),
+    b = prompt('Во сколько обойдется ?', '');
+
+  if (
+    typeof a === 'string' &&
+    typeof a != null &&
+    typeof b != null &&
+    a != '' &&
+    b != '' &&
+    a.length < 50
+  ) {
+    console.log('done');
+    appData.expenses[a] = b;
+  }
+}
+appData.moneyPerDay = appData.budget / 30;
+
+alert('бюджет на 1 день - ' + appData.moneyPerDay);
+
+if (appData.moneyPerDay < 500) {
+  console.log('Минимальный уровень достатка');
+} else if (appData.moneyPerDay > 500 && appData.moneyPerDay < 1000) {
+  console.log('Средний уровень достатка');
+} else if (appData.moneyPerDay > 1000) {
+  console.log('Высокий уровень достатка');
+} else {
+  console.log('Error!');
+}
