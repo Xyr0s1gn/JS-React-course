47c09057f9625dda0e44e7c1343325e610a1f9b7
Bino Faata <maximleluk@gmail.com>
2020-05-16 20:17:14 +0300

    Learning a functions

--- Head_First_JavaScript/other_lessons/script.js
+++ Head_First_JavaScript/other_lessons/script.js
@@ -1,13 +1,25 @@
-let soldier = {
-  health: 400,
-  armor: 100,
-};
+// let soldier = {
+//   health: 400,
+//   armor: 100,
+// };
 
-let john = {
-  health: 100,
-};
+// let john = {
+//   health: 100,
+// };
 
-john.__proto__ = soldier;
+// john.__proto__ = soldier;
 
-console.log(john);
-console.log(john.armor);
+// console.log(john);
+// console.log(john.armor);
+
+// What should i wear. (A function test)
+let temperature, temp;
+function whatShallIWear(temp) {
+  if (temp < 60) {
+    console.log('wear a jacket');
+  } else if (temp < 70) {
+    console.log('wear a sweater');
+  } else {
+    console.log('wear a t-shirt');
+  }
+}
