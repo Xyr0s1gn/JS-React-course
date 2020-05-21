aa29456b7f00b39490f1092114082f88bf96767a
Bino Faata <maximleluk@gmail.com>
2020-05-21 17:01:29 +0300

    New chapter. Learning arrays

--- Head_First_JavaScript/other_lessons/script.js
+++ Head_First_JavaScript/other_lessons/script.js
@@ -75,15 +75,19 @@
 // thingamajig(5);
 // console.log(clunkCounter);
 ////////////////////////////////////////////
-var balance = 10500;
-var cameraOn = true;
-function steal(balance, amount) {
-  cameraOn = false;
-  if (amount < balance) {
-    balance = balance - amount;
-  }
-  return amount;
-  cameraOn = true;
-}
-var amount = steal(balance, 1500);
-alert('Criminal: you stole ' + amount + '!');
+// var balance = 10500;
+// var cameraOn = true;
+// function steal(balance, amount) {
+//   cameraOn = false;
+//   if (amount < balance) {
+//     balance = balance - amount;
+//   }
+//   return amount;
+//   cameraOn = true;
+// }
+// var amount = steal(balance, 1500);
+// alert('Criminal: you stole ' + amount + '!');
+//
+////////////////////////////////////////////
+// Arrays
+////////////////////////////////////////////
