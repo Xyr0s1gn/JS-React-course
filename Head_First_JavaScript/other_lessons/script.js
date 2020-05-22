49e65819df6205a0bdda430cb90712919ef06bfb
Bino Faata <maximleluk@gmail.com>
2020-05-23 00:17:13 +0300

    Learning DOM

--- Head_First_JavaScript/other_lessons/script.js
+++ Head_First_JavaScript/other_lessons/script.js
@@ -1,3 +1,4 @@
+'use strict';
 // let soldier = {
 //   health: 400,
 //   armor: 100,
@@ -238,10 +239,149 @@
 //   }
 //   return index;
 // }
-// let mostCostEffective = getMostCostEffectiveSolution(scores, costs, highScore);
+//let mostCostEffective = getMostCostEffectiveSolution(scores, costs,highScore);
 // console.log(
 //   'Bubble Solution #' + mostCostEffective + ' is the most cost effective'
 // );
 //////////////////////////////////////////////////////////////////////////
 // OBJECTS
 //////////////////////////////////////////////////////////////////////////
+// let taxi = {
+//   make: 'Webville Motors',
+//   model: 'Taxi',
+//   year: 1955,
+//   color: 'yellow',
+//   passengers: 4,
+//   convertible: false,
+//   mileage: 281341,
+// };
+
+// let cadi = {
+//   make: 'GM',
+//   model: 'Cadillac',
+//   year: 1955,
+//   color: 'tan',
+//   passengers: 5,
+//   convertible: false,
+//   mileage: 12892,
+// };
+
+// let fiat = {
+//   make: 'Fiat',
+//   model: '500',
+//   year: 1957,
+//   color: 'Medium Blue',
+//   passengers: 2,
+//   convertible: false,
+//   mileage: 88000,
+// };
+
+// let chevy = {
+//   make: 'Chevy',
+//   model: 'Bel Air',
+//   year: 1957,
+//   color: 'red',
+//   passengers: 2,
+//   convertible: false,
+//   mileage: 1021,
+// };
+
+// function prequal(car) {
+//   if (car.mileage > 10000) {
+//     return false;
+//   } else if (car.year > 1960) {
+//     return false;
+//   }
+//   return true;
+// }
+
+// function lookACar(car) {
+//   prequal(car);
+//   if (prequal(car)) {
+//     console.log('You gotta check out this ' + car.make + ' ' + car.model);
+//   } else {
+//     console.log('You should pass on the ' + car.make + ' ' + car.model);
+//   }
+// }
+
+// lookACar(taxi);
+// lookACar(cadi);
+// lookACar(fiat);
+// lookACar(chevy);
+////////////////////////////////////////////////////////
+// function makeCar() {
+//   let makes = ['Chevy', 'GM', 'Fiat', 'Webville Motors', 'Tucker'];
+//   let models = ['Cadillac', '500', 'Bel-Air', 'Taxi', 'Torpedo'];
+//   let years = [1955, 1957, 1948, 1954, 1961];
+//   let colors = ['red', 'blue', 'tan', 'yellow', 'white'];
+//   let convertible = [true, false];
+
+//   let rand1 = Math.floor(Math.random() * makes.length);
+//   let rand2 = Math.floor(Math.random() * models.length);
+//   let rand3 = Math.floor(Math.random() * years.length);
+//   let rand4 = Math.floor(Math.random() * colors.length);
+//   let rand5 = Math.floor(Math.random() * 5 + 1);
+//   let rand6 = Math.floor(Math.random() * 2);
+
+//   let car = {
+//     make: makes[rand1],
+//     model: models[rand2],
+//     year: years[rand3],
+//     color: colors[rand4],
+//     passengers: rand5,
+//     convertible: convertible[rand6],
+//     mileage: 0,
+//   };
+
+//   return car;
+// }
+
+// function displayCar(car) {
+//   console.log(
+//     'Your new car is a ' + car.year + ' ' + car.make + ' ' + car.model
+//   );
+// }
+
+// for (let i = 0; i < 5; i++) {
+//   let carToSell = makeCar();
+//   displayCar(carToSell);
+// }
+//////////////////////////////////////////////////////////////////////
+
+let fiat = {
+  make: 'Fiat',
+  model: '500',
+  year: 1957,
+  color: 'Medium Blue',
+  passengers: 2,
+  convertible: false,
+  mileage: 88000,
+  started: false,
+  fuel: 0,
+  start: function () {
+    if (this.fuel != 0) {
+      this.started = true;
+    } else {
+      console.log('The car is on empty, fill up before starting!');
+    }
+  },
+  stop: function () {
+    this.started = false;
+  },
+  drive: function () {
+    if (this.started) {
+      if (this.fuel > 0) {
+        console.log(this.make + ' ' + this.model + ' goes zoom zoom!');
+        this.fuel = this.fuel - 1;
+      } else {
+        console.log('Uj oh, out of fuel.');
+        this.stop();
+      }
+    } else {
+      console.log('You need to start the engine first.');
+    }
+  },
+  addFuel: function (amount) {
+    this.fuel = this.fuel + amount;
+  },
+};
