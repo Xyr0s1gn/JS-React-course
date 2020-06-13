8841698b0f31d08cd4f1fe836917c9995e18c528
Bino Faata <maximleluk@gmail.com>
2020-06-13 20:28:44 +0300

    Startet Object constructors

---
+++ Head_First_JavaScript/other_lessons/script6.js
@@ -0,0 +1,24 @@
+'use strict';
+// function Duck(type, canFly) {
+//   this.type = type;
+//   this.canFly = canFly;
+// }
+///////////////////////////////////
+function Dog(name, breed, weight) {
+  this.name = name;
+  this.breed = breed;
+  this.weight = weight;
+}
+
+let fido = new Dog('Fido', 'Mixed', 38);
+let fluffy = new Dog('Fluffy', 'Poodle', 30);
+let spot = new Dog('Spot', 'Chihuahua', 10);
+
+let dogs = [fido, fluffy, spot];
+for (let i = 0; i < dogs.length; i++) {
+  let size = 'small';
+  if (dogs[i].weight > 10) {
+    size = 'large';
+  }
+  console.log('Dog: ' + dogs[i].name + ' is a ' + size + ' ' + dogs[i].breed);
+}
