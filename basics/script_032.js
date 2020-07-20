c0e971a4cce2bf03c0089d8198b3120da513810b
Bino Faata <maximleluk@gmail.com>
2020-07-20 15:32:35 +0300

    dom

--- basics/script_032.js
+++ basics/script_032.js
@@ -1,3 +1,6 @@
+/* eslint-disable eqeqeq */
+/* eslint-disable no-continue */
+
 // console.log(document.body);
 // console.log(document.body.childNodes);
 // console.log(document.body.firstChild);
@@ -5,4 +8,13 @@
 
 // console.log(document.querySelector('#current').parentNode);
 
-console.log(document.querySelector('[data-current="3"]'));
+// console.log(document.querySelector('[data-current="3"]').parentElement);
+
+// console.log(document.body.firstElementChild);
+
+for (const node of document.body.childNodes) {
+  if (node.nodeName == '#text') {
+    continue;
+  }
+  console.log(node);
+}
