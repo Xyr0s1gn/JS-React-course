f09232e54c1d07d2595225c54bbd086324aebcd6
Bino Faata <maximleluk@gmail.com>
2020-07-27 00:51:52 +0300

    anim, setinterval, settimeout

--- js_in_progress/samples/script.js
+++ js_in_progress/samples/script.js
@@ -1,4 +1,41 @@
-const btns = document.querySelectorAll('button');
+// const btns = document.querySelectorAll('button');
+// const wrapper = document.querySelector('.btn-block');
 
 // console.log(btns[0].classList.length);
-console.log(btns[2].classList.add('red'));
+// console.log(btns[2].classList.add('red'));
+// console.log(btns[0].classList.remove('some'));
+
+// btns[0].addEventListener('click', () => {
+//   if (!btns[1].classList.contains('red')) {
+//     btns[1].classList.add('red');
+//   } else {
+//     btns[1].classList.remove('red');
+//   }
+//   btns[4].classList.toggle('red');
+//   btns[5].classList.toggle('red');
+// });
+
+// wrapper.addEventListener('click', event => {
+//   if (event.target && event.target.matches('button.red')) {
+//     console.log('hello');
+//   }
+// });
+
+// THIS WILL NOT WORK
+// btns.forEach(btn => {
+//   btn.addEventListener('click', () => {
+//     console.log('another');
+//   });
+// });
+
+// const btn = document.createElement('button');
+// btn.classList.add('red');
+// wrapper.append(btn);
+
+// function logger() {
+//   console.log('text');
+// }
+
+// const timerId = setTimeout(logger, 2000);
+
+// clearInterval(timerId);
