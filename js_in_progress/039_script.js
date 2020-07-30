c31903e3d4f4dc0da8f102dc269c51aaea4b222b
Bino Faata <maximleluk@gmail.com>
2020-07-30 22:03:26 +0300

    Classes

--- js_in_progress/039_script.js
+++ js_in_progress/039_script.js
@@ -51,3 +51,127 @@
 // const height = box.offsetHeight;
 
 // console.log(width, height);
+
+// ///////////////////////////////////////////
+// Constructors
+// ///////////////////////////////////////////
+// function User(name, id) {
+//   this.name = name;
+//   this.id = id;
+//   this.human = true;
+//   this.hello = function() {
+//     if (this.human) {
+//       console.log(`Hello ${this.name}!`);
+//       console.log(`${this.name} is human.`);
+//     } else {
+//       console.log(`Alarm! Alarm! Alarm!`);
+//       console.log(`${this.name} is alien!`);
+//     }
+//   };
+// }
+
+// const bertran = new User('Bertran', '0');
+// const alex = new User('Alex', '2');
+
+// alex.human = false;
+
+// bertran.hello();
+// alex.hello();
+
+// User.prototype.exit = function() {
+//   console.log(`${this.name} was leave area 51`);
+// };
+
+// alex.exit();
+// ///////////////////////////////////////////
+// function showThis(a, b) {
+//   console.log(this);
+//   function sum() {
+//     console.log(this);
+//     return a + b;
+//   }
+//   console.log(sum());
+// }
+// showThis(4, 3);
+
+// const obj = {
+//   a: 20,
+//   b: 15,
+//   sum() {
+//     console.log(this);
+//   },
+// };
+// obj.sum();
+
+// function sayName() {
+//   console.log(this);
+//   console.log(this.name);
+// }
+
+// const user = {
+//   name: 'John',
+// };
+
+// sayName.call(user);
+// sayName.apply(user);
+
+// function count(num) {
+//   return this * num;
+// }
+
+// const double = count.bind(2);
+
+// console.log(double(3));
+// console.log(double(13));
+
+// const btn = document.querySelector('button');
+// btn.addEventListener('click', function() {
+//   this.style.backgroundColor = 'brown';
+// });
+
+// const obj = {
+//   num: 5,
+//   sayNumber() {
+//     const say = () => {
+//       console.log(this);
+//     };
+//     say();
+//   },
+// };
+
+// obj.sayNumber();
+
+// const double = a => a * 2;
+
+class Rectangle {
+  constructor(height, width) {
+    this.height = height;
+    this.width = width;
+  }
+
+  calcArea() {
+    return this.height * this.width;
+  }
+}
+
+class ColoredRectangleWithText extends Rectangle {
+  constructor(height, width, text, bgColor) {
+    super(height, width);
+    this.text = text;
+    this.bgColor = bgColor;
+  }
+
+  showMyProps() {
+    console.log(`Text: ${this.text}, color: ${this.bgColor}`);
+  }
+}
+
+const div = new ColoredRectangleWithText(25, 19, 'Hello, World', 'red');
+
+div.showMyProps();
+console.log(div.calcArea());
+// const square = new Rectangle(10, 10);
+// const anotherSquare = new Rectangle(25, 14);
+
+// console.log(` ${square.calcArea()}`);
+// console.log(`${anotherSquare.calcArea()}`);
